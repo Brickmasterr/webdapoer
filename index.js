9ecb0631f62f21e47737fe654c1242105404ec8a
Brickmasterr <41365639+Brickmasterr@users.noreply.github.com>
2024-06-24 18:23:43 +0000

    create index for starter

---
+++ index.js
@@ -0,0 +1,14 @@
+
+require('events').EventEmitter.defaultMaxListeners = 1000;
+
+const config = require('./src/config');
+const MYSQL_CLASS = require('./src/handler/database/DBUtil');
+const connection = new MYSQL_CLASS(config.dbConfig);
+connection.connect(config.dbConfig, function() {
+    global.mysql_connection = connection;
+
+    require('./src/server');
+});
+
+process.on('unhandledRejection', e => console.error(e))
+    .on('uncaughtException', e => console.error(e));
